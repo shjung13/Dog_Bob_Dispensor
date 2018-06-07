1466a3bc3be70279bccf6fbcc87d84e98e747389
j5950 <zaqwsx3255@naver.com>
2018-06-07 19:25:44 +0900

    Update Servo.ino

--- Servo.ino
+++ Servo.ino
@@ -13,43 +13,35 @@ void setup()
     //Serial.println("hello arduino!");
     microServo.attach(servoPin);   
 }
-
-void loop()
-{
-    bool feedtoggle = false;
-    float current_weight; 
+void feed(){
     int angle;
-    if(current_weight < 100)
-        feedtoggle = true;
-    if(feedtoggle){        
-        while(angle == MAX_DEGREE){
+    while(angle < MAX_DEGREE){
 
-            //Check current_weight
-            //current_weight = get_weight_data();
-            if(current_weight < MAX_AMOUNT_OF_BOB){
+        //Check current_weight
+        current_weight = scale.getGram();
+        if(current_weight > MAX_AMOUNT_OF_BOB){
 
-                //return slowly
-                while(angle != 0){
-                    microServo.write(angle--);
-                    delay(10);
-                }
-
-                //return directly    
-                angle = 0;
-                microServo.write(angle);
-                
-                break;
+            //return slowly
+            while(angle != 0){
+                microServo.write(angle--);
+                delay(10);
             }
-            microServo.write(angle++);          
-            delay(10);                    
-        }
 
-        while((current_weight < MAX_AMOUNT_OF_BOB) && (angle != 0)){
-            //current_weight = get_weight_data();
-            delay(10);        
-        }
+            //return directly    
+            angle = 0;
+            microServo.write(angle);
 
-        while(angle != 0)
-            microServo.write(angle--);
+            break;
+        }
+        microServo.write(angle++);          
+        delay(10);                    
     }
+
+    while((current_weight < MAX_AMOUNT_OF_BOB) && (angle != 0)){
+        current_weight = scale.getGram();
+        delay(10);        
+    }
+
+    while(angle != 0)
+        microServo.write(angle--);
 }
